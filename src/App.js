91424f2f00b6a8392e579eacb3f4401208f1e676
priyalp2000 <105322713+priyalp2000@users.noreply.github.com>
2023-02-01 16:38:18 -0400

    Updated App.js

--- src/App.js
+++ src/App.js
@@ -6,8 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Welcome to React world !!!
-          Learn React !!!
+        <p>Hello World!!!
         </p>
       </header>
     </div>
